f587acf15eba8c63fe4e5d863d59a37c1c5241d0
dorjepadma <dorjepadma@gmail.com>
2022-02-04 11:41:14 -0800

    saturnia.io launch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import SaturnSigil from './assets/images/saturn_sigil.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={SaturnSigil} className="App-logo" alt="logo" />
+        <p>
+        Saturnia.io
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
